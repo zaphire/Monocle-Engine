bcdc011c191281e24ef9b0e44902f1ba458e39f0
Matt <matt@mattmakesgames.com>
2011-03-14 16:35:55 -0600

    Entity.cpp

---
+++ Code/Entity.cpp
@@ -0,0 +1,34 @@
+#include "Entity.h"
+
+namespace Monocle
+{
+	Entity::Entity()
+	{
+
+	}
+
+	Entity::~Entity()
+	{
+
+	}
+
+	void Entity::Added()
+	{
+
+	}
+
+	void Entity::Removed()
+	{
+
+	}
+
+	void Entity::Update()
+	{
+
+	}
+
+	void Entity::Render()
+	{
+
+	}
+}
