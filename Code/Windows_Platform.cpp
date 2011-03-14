d840322291673d4e6d0b2a787f894708be4eaf5b
Alec Holowka <alec@infiniteammo.ca>
2011-03-14 15:16:31 -0500

    syncing 2 files

---
+++ Code/Windows_Platform.cpp
@@ -0,0 +1,20 @@
+#include "Platform.h"
+
+namespace Monocle
+{
+
+	Platform::Platform()
+	{
+	}
+
+	void Platform::Init()
+	{
+		// make a default window?
+	}
+
+	bool Platform::IsKeyPressed(KeyCode keyCode)
+	{
+		// do fugly windows key stuff
+		return false;
+	}
+}
