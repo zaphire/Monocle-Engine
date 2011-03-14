631172d8ac35469a63fa0538875433136aab3e0b
Matt <matt@mattmakesgames.com>
2011-03-14 15:16:40 -0600

    Added Entity.h, added entity list to Scene.h

---
+++ Code/Entity.h
@@ -0,0 +1,12 @@
+#pragma once
+
+using namespace Monocle
+{
+	public class Entity
+	{
+	public:
+		Entity();
+		void update();
+		void render();
+	};
+}
--- Code/Scene.h
+++ Code/Scene.h
@@ -1,12 +1,20 @@
 #pragma once
 
+#include <list>
+
+using std::list;
+
 namespace Monocle
 {
+	class Entity;
+
 	class Scene
 	{
 	public:
 		Scene();
 		void update();
 		void render();
+	private:
+		list<Entity*> entities;
 	};
 }
