eab00a7784ff26bb07554fe5079444a1cef7341e
Linearity <u0708549@utah.edu>
2011-03-14 20:26:51 -0700

    Edited operators, changed to non-inline for early-stage code sanity

--- Code/Vector2.h
+++ Code/Vector2.h
@@ -14,7 +14,6 @@ namespace Monocle
 		static Vector2 left;
 		static Vector2 right;
 		
-		Vector2();
 		Vector2(float x, float y);
 
 		float GetSquaredMagnitude();
@@ -26,6 +25,7 @@ namespace Monocle
 		void Clamp(float max);
 		bool IsInRange(float range);
 		float Dot(Vector2 other);
+		float Cross(Vector2 other);
 		
 		/*
 		static Vector2 Add(Vector2 a, Vector2 b);
@@ -33,20 +33,27 @@ namespace Monocle
 		static Vector2 Multiply(Vector2 a, float s);
 		static Vector2 Scale(Vector2 a, float s);
 		*/
+		
+		float operator[](int i);
+		
+		Vector2& operator=(const Vector2& rhs);
+		bool operator==(const Vector2& rhs);
+		bool operator!=(const Vector2& rhs);
+	
+		Vector2& operator+=(const Vector2& rhs);
+		Vector2& operator-=(const Vector2& rhs);
+		Vector2& operator*=(float rhs);				// scalar multiplication
+		Vector2& operator/=(float rhs);				// scalar inverse multiplication
 
-		inline Vector2 operator+=(const Vector2& rhs);	
-		inline Vector2 operator-=(const Vector2& rhs);
-		inline Vector2 operator*=(float rhs);					// scalar multiplication
-		inline Vector2 operator/=(float rhs);					// scalar inverse multiplication
 	};
 
-	inline Vector2 operator+(const Vector2& lhs, const Vector2& rhs);	
-	inline Vector2 operator-(const Vector2& lhs, const Vector2& rhs);
-
-	inline Vector2 operator*(float lhs, const Vector2& rhs);			// left scalar multiplication
-	inline Vector2 operator*(const Vector2& lhs, float rhs);			// right scalar multiplication
-	inline Vector2 operator/(const Vector2& lhs, float rhs);			// right scalar inverse multiplication
+	Vector2 operator+(const Vector2& lhs, const Vector2& rhs);	
+	Vector2 operator-(const Vector2& lhs, const Vector2& rhs);
 	
-	inline float operator*(const Vector2& lhs, const Vector2& rhs);	// dot product
-	inline float operator^(const Vector2& lhs, const Vector2& rhs);	// cross product
+	Vector2 operator*(float lhs, const Vector2& rhs);		// left scalar multiplication
+	Vector2 operator*(const Vector2& lhs, float rhs);		// right scalar multiplication
+	Vector2 operator/(const Vector2& lhs, float rhs);		// right scalar inverse multiplication
+	
+	float operator*(const Vector2& lhs, const Vector2& rhs);	// dot product
+	float operator^(const Vector2& lhs, const Vector2& rhs);	// cross product
 }
