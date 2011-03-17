07f71e7477ded0789ddb36220dbbf3f8b35cf692
Alec Holowka <alec@infiniteammo.ca>
2011-03-17 15:48:11 -0500

    SpriteAnimation placeholder.

---
+++ Code/Graphics/SpriteAnimation.cpp
@@ -0,0 +1,5 @@
+#include "SpriteAnimation.h"
+
+namespace Monocle
+{
+}
---
+++ Code/Graphics/SpriteAnimation.h
@@ -0,0 +1,11 @@
+#include "Sprite.h"
+
+namespace Monocle
+{
+	class SpriteAnimation : public Sprite
+	{
+	public:
+		//SpriteAnimation();
+		//void Update();
+	};
+}
