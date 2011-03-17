725cf220e4e7f9811762c2cf82164d1e6dc5bb46
Guillermo A. Amaral <g@maral.me>
2011-03-17 11:42:17 -0700

    Easy build script (UNIX)

---
+++ cmake-build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+mkdir -p Build
+pushd Build
+
+echo "Do you want to build tests? (y/n) : "
+read ANSWER
+
+if [ "${ANSWER}" == "y" || "${ANSWER}" == "Y" ]; then
+	CMAKEOPTS="${CMAKEOPTS} -DBUILD_TESTS:BOOL=ON"
+else
+	CMAKEOPTS="${CMAKEOPTS} -DBUILD_TESTS:BOOL=OFF"
+fi
+
+cmake ${CMAKEOPTS} ..
+
+popd
+make -C Build/ all
+
