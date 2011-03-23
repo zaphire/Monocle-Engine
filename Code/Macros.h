5ce5c2ab967f464145da6835a46e1bf06904ef95
Matt Thorson <matt@mattmakesgames.com>
2011-03-22 18:10:57 -0600

    Added some more useful math macros

--- Code/Macros.h
+++ Code/Macros.h
@@ -1,4 +1,11 @@
 #pragma once
 
+//Standard math min/max macros
 #define MIN(a, b) (((a) < (b)) ? (a) : (b)) 
 #define MAX(a, b) (((a) > (b)) ? (a) : (b)) 
+
+//If value is larger than zero, return to. If value is smaller than zero, return -to. If value is zero, return zero.
+#define SIGN(value, to) ((value < 0) ? -to : ((value > 0) ? to : 0))
+
+//Returns value shifted toward target by amount, without crossing target
+#define APPROACH(value, target, amount) ((value >= target) ? MAX(value - amount, target) : MIN(value + amount, target))
