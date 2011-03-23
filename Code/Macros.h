bd416bda8034ca70e68a6129c946a8e5614300e4
Matt Thorson <matt@mattmakesgames.com>
2011-03-22 18:33:50 -0600

    Added more macros and commented all macros

--- Code/Macros.h
+++ Code/Macros.h
@@ -1,11 +1,35 @@
 #pragma once
 
-//Standard math min/max macros
+/*	MIN and MAX
+		Standard math min/max macros.
+		MIN returns the smaller of the arguments.
+		MAX returns the larger of the arguments.
+*/
 #define MIN(a, b) (((a) < (b)) ? (a) : (b)) 
 #define MAX(a, b) (((a) > (b)) ? (a) : (b)) 
 
-//If value is larger than zero, return to. If value is smaller than zero, return -to. If value is zero, return zero.
+/*	CLAMP
+		Combines min and max in one macro for convenience.
+*/
+#define CLAMP(a, min, max) MIN(MAX(a, max), min)
+
+/* SIGN
+		If value is larger than 0, return to. 
+		If value is smaller than 0, return -to. 
+		If value is 0, return 0.
+*/
 #define SIGN(value, to) ((value < 0) ? -to : ((value > 0) ? to : 0))
 
-//Returns value shifted toward target by amount, without crossing target
+/*	APPROACH
+		Returns value shifted toward target by amount, without crossing target.
+		For example, APPROACH(2, 0, 3) will perform 2 - 3 = -1, but return 0 because -1 crossed the target of 0.
+		APPROACH(-4, 0, 3) will return -1, because it will perform -4 + 3 = -1.
+*/
 #define APPROACH(value, target, amount) ((value >= target) ? MAX(value - amount, target) : MIN(value + amount, target))
+
+/*	LERP
+		Linearly interpolate between two values.
+		For example LERP(0, 10, 0.5) = 5, because 5 is 50% between 0 and 10.
+		LERP(0, -4, 0.75) = -3, because -3 is 75% between 0 and -4.
+*/
+#define LERP(a, b, t) (a + (b - a) * t)
