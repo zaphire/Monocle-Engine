a09323af59c82c850acdb4d63efd41632861d40b
Alec Holowka <alec@infiniteammo.ca>
2011-04-19 22:06:35 -0500

    ImageBrowser::SetImage added, fixed Linux version of ForEachFile

--- Code/LevelEditor/ImageBrowser.h
+++ Code/LevelEditor/ImageBrowser.h
@@ -4,28 +4,29 @@
 
 namespace Monocle
 {
-    class SelectionImage : public Entity
-    {
-    public:
-        SelectionImage(const std::string &image, int size);
-	void Update();
-    };
+	class SelectionImage : public Entity
+	{
+		public:
+			SelectionImage(const std::string &image, int size);
+			void Update();
+	};
 
 	class ImageBrowser : public Entity
 	{
-	public:
-	    ImageBrowser();
-        void Update();
-        //void Render();
-		void ScanDirectory(const std::string &directory);
-	
-	private:
-		static void FileCallback(const std::string &filename, intptr_t pointer);
-		void NewSelectionImage(const std::string &filename);
-		
-		int selectionImageSize;
-		bool hasContent;
-		std::list<SelectionImage*> selectionImages;
-		Entity *grid;
+		public:
+			ImageBrowser();
+			void Update();
+			//void Render();
+			void ScanDirectory(const std::string &directory);
+			void SetImages(const std::vector<std::string> &images);
+
+		private:
+			static void FileCallback(const std::string &filename, intptr_t pointer);
+			void NewSelectionImage(const std::string &filename);
+
+			int selectionImageSize;
+			bool hasContent;
+			std::list<SelectionImage*> selectionImages;
+			Entity *grid;
 	};
 }
