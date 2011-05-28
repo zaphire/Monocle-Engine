a9cfea0989c2082f1ad3642781142a2e321b34d3
Martyn York <martiny@sfu.ca>
2011-05-27 18:06:53 -0700

    added Mock dir

---
+++ Tests/Unit/Mock/Game.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <Game.h>
+
+namespace Monocle
+{
+    class FontAsset;
+    
+    namespace Test
+    {
+        namespace Unit
+        {
+            namespace Mock
+            {
+                
+            }
+        }
+    }
+}
