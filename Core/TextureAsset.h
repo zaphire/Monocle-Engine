a05312cf83dbfeb780db97e419f0e9c47cad3a3e
Martyn York <martiny@sfu.ca>
2011-05-29 09:13:56 -0700

    added ability to pre-allocate space for a texture using a pointer, update a portion of a texture from data, or copy a portion of a texture.

--- Core/TextureAsset.h
+++ Core/TextureAsset.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Asset.h"
+#include "Vector2.h"
 
 namespace Monocle
 {
@@ -8,13 +9,26 @@ namespace Monocle
 	{
 	public:
 		TextureAsset();
+		//Loads a texture from a pointer.
+		//The data pointer may be null, in which case UpdateRect can later be used to fill the texture with
+		//an image.
+		void Load(const unsigned char* data, int w, int h, FilterType filter, bool repeatX, bool repeatY);
 		bool Load(const std::string &filename, FilterType filter, bool repeatX, bool repeatY);
+        //Replaces the pixels in a rectangular area of the texture using the data pointed to.
+		void UpdateRect(const unsigned char *data, Monocle::Vector2 position, Monocle::Vector2 size);
+        //Copies a portion of the texture into another area of the same texture.
+		void CopyRect(Monocle::Vector2 srcPos, Monocle::Vector2 dstPos, Monocle::Vector2 copysize);
 		void Reload();
 		void Unload();
+
+        //Copies a portion of the texture into a buffer, and returns it.
+        unsigned char *ReadRect(Monocle::Vector2 srcPos, Monocle::Vector2 size);
 
 		FilterType filter;
 		bool repeatX, repeatY;
 		unsigned int width, height;
 		unsigned int texID;
+
+    private:
 	};
 }
