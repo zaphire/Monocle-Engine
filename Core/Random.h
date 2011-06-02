01407baee13c65854c98b6bc86543d2ffb6b1412
Alec Holowka <alec@infiniteammo.ca>
2011-06-02 12:57:44 -0500

    comment

--- Core/Random.h
+++ Core/Random.h
@@ -7,6 +7,7 @@ namespace Monocle
 	public:
 		Random();
 		static void Seed(unsigned int seed);
+		//! not inclusive
 		static int Int(int start, int end);
 		static float Float(float start, float end);
 		static float Percent();
