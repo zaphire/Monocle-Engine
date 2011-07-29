98e002ce15ffad98f1d80c1a6351a3130e37833b
Josh Whelchel <josh@jwmusic.org>
2011-07-29 11:52:17 -0700

    More updates for iPhone/iPad compatibility. Adding Touch inputs for a few tests (Ogmo, TestSelector and Pong). The Touch input system has been added as well. A few remaining bugs with Audio on the iOS. Also a minor fix for Ogmo::Player not calling Entity::Update() super.

---
+++ Samples/TestSelector/TestSelector.h
@@ -0,0 +1,14 @@
+//
+//  TestSelector.h
+//  MonocleIOS
+//
+//  Created by Josh Whelchel on 7/28/11.
+//  Copyright 2011 jwmusic.org. All rights reserved.
+//
+
+#ifndef MonocleIOS_TestSelector_h
+#define MonocleIOS_TestSelector_h
+
+
+
+#endif
